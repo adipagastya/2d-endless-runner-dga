69a312e237685de3ccc1a4107136b71590747122
adipagastya <adipa.pt@gmail.com>
2021-04-21 19:29:28 +0800

    First commit

---
+++ Assets/Scripts/CharacterMoveController.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMoveController : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveAccel;
+    public float maxSpeed;
+
+    [Header("Jump")]
+    public float jumpAccel;
+
+    [Header("Ground Raycast")]
+    public float groundRaycastDistance;
+    public LayerMask groundLayerMask;
+
+    [Header("Scoring")]
+    public ScoreController score;
+    public float scoringRatio;
+
+    [Header("GameOver")]
+    public GameObject gameOverScreen;
+    public float fallPositonY;
+
+    [Header("Camera")]
+    public CameraMoveController gameCamera;
+
+    private Rigidbody2D rig;
+
+    private bool isJumping;
+    private bool isOnGround;
+
+    private float lastPositionX;
+
+    private Animator anim;
+    private CharacterSoundController sound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sound = GetComponent<CharacterSoundController>();
+
+        lastPositionX = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Read input
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(isOnGround)
+            {
+                isJumping = true;
+
+                sound.PlayJump();
+            }
+        }
+
+        // Change animation
+        anim.SetBool("isOnGround", isOnGround);
+
+        // Calculate score
+        int distancePassed = Mathf.FloorToInt(transform.position.x - lastPositionX);
+        int scoreIncrement = Mathf.FloorToInt(distancePassed / scoringRatio);
+
+        if(scoreIncrement > 0)
+        {
+            score.IncreaseCurrentScore(scoreIncrement);
+            lastPositionX += distancePassed;
+        }
+        
+        // Raycast ground
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, groundRaycastDistance, groundLayerMask);
+        if (hit)
+        {
+            if (!isOnGround && rig.velocity.y <= 0)
+            {
+                isOnGround = true;
+            }
+        }
+        else
+        {
+            isOnGround = false;
+        }
+
+        if(transform.position.y < fallPositonY)
+        {
+            GameOver();
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        // Calculate velocity vector
+        Vector2 velocityVector = rig.velocity;
+
+        if(isJumping)
+        {
+            velocityVector.y += jumpAccel;
+            isJumping = false;
+        }
+
+        velocityVector.x = Mathf.Clamp(velocityVector.x + moveAccel * Time.deltaTime, 0.0f, maxSpeed);
+
+        rig.velocity = velocityVector;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Debug.DrawLine(transform.position, transform.position + (Vector3.down * groundRaycastDistance), Color.white);
+    }
+
+    private void GameOver()
+    {
+        // Set high score
+        score.FinishScoring();
+
+        // Stop camera movement
+        gameCamera.enabled = false;
+
+        // Show gamover
+        gameOverScreen.SetActive(true);
+
+        // Disable this too
+        this.enabled = false;
+    }
+
+}
---
+++ Assets/Scripts/ScoreController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreController : MonoBehaviour
+{
+    [Header("Scoring")]
+    public ScoreController score;
+    public float scoringRatio;
+    private float lastPositionX;
+
+    private int currentScore = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Reset
+        currentScore = 0;
+    }
+
+    public float GetCurrentScore()
+    {
+        return currentScore;
+    }
+
+    public void IncreaseCurrentScore(int increment)
+    {
+        currentScore += increment;
+    }
+
+    public void FinishScoring()
+    {
+        // Set high score
+        if(currentScore > ScoreData.highScore)
+        {
+            ScoreData.highScore = currentScore;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Read input
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(isOnGround)
+            {
+                isJumping = true;
+
+                sound.PlayJump();
+            }
+        }
+
+        // Change animation
+        anim.SetBool("isOnGround", isOnGround);
+
+        // Calculate score
+        int distancePassed = Mathf.FloorToInt(transform.position.x - lastPositionX);
+        int scoreIncrement = Mathf.FloorToInt(distancePassed / scoringRatio);
+
+        if(scoreIncrement > 0)
+        {
+            score.IncreaseCurrentScore(scoreIncrement);
+            lastPositionX += distancePassed;
+        }
+    }
+}
